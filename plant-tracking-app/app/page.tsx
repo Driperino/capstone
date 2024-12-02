fed02c8a858ff26ee47ef8c8d152668cd85577d1
Vincent <vincent.winkler98@gmail.com>
2024-12-02 15:51:32 -0800

    refactor

--- plant-tracking-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  // const isLoggedIn = false;
-  return (
-    <div>
-      <h1>Welcome to the Plant Tracking App!</h1>
-      <a href="/api/auth/login">Login</a>
-    </div>
-  );
-}
